98c49a41b366d707184c7b6defb4466e3636a79d
Alex Dalton <alex.k.dalton@gmail.com>
2016-10-25 21:43:32 +0100

    parallel in av_velocity

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -401,6 +401,7 @@ double av_velocity(const t_param params, t_speed* cells, int* obstacles)
   /* initialise */
   tot_u = 0.0;
 
+#pragma omp parallel for reduction (+:tot_u,tot_cells) private(ii,jj)
   /* loop over all non-blocked cells */
   for (int ii = 0; ii < params.ny; ii++)
   {
