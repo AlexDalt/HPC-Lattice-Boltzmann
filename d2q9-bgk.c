39fb44c07116ee7b823f9743502ecda062d3f208
Alex Dalton <alex.k.dalton@gmail.com>
2016-10-26 23:12:39 +0100

    step length 16

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -60,7 +60,7 @@
 #define NSPEEDS         9
 #define FINALSTATEFILE  "final_state.dat"
 #define AVVELSFILE      "av_vels.dat"
-#define STEP            8
+#define STEP            16
 #define NUM_THREADS     16 
 
 /* struct to hold the parameter values */
