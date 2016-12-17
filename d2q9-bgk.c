90495537a74e0fb0b858ec6549722241f24046b8
Alex Dalton <alex.k.dalton@gmail.com>
2016-12-17 16:36:07 +0000

    error fixing

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -85,7 +85,7 @@ int finalise(const t_param* params, t_speed** cells_ptr, t_speed** tmp_cells_ptr
 float total_density(const t_param params, t_speed* cells);
 
 /* compute average velocity */
-float av_velocity(const t_param params, t_speed* cells, int* obstacles, t_ocl ocl);
+float av_velocity(const t_param params, t_ocl ocl, int tot_cells);
 
 /* calculate Reynolds number */
 float calc_reynolds(const t_param params, t_speed* cells, int* obstacles, t_ocl ocl);
@@ -330,7 +330,7 @@ int collision(const t_param params, t_speed* cells, t_speed* tmp_cells, int* obs
   return EXIT_SUCCESS;
 }
 
-float av_velocity(const t_param params, t_ocl ocl, int tot_cells;)
+float av_velocity(const t_param params, t_ocl ocl, int tot_cells)
 {
   int    tot_cells = 0;  /* no. of cells used in calculation */
   float tot_u = 0;          /* accumulated magnitudes of velocity for each cell */
