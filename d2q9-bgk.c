171fc9d078f3e11b69df4705d227b6b570105a8a
Alex Dalton <alex.k.dalton@gmail.com>
2016-12-19 03:52:35 +0000

    using 12 compute units

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -260,7 +260,7 @@ float comp_func(const t_param params, cl_mem* cells, cl_mem* tmp_cells, t_ocl oc
   checkError(err, "setting comp_func arg 6", __LINE__);
 
   // Enqueue kernel
-  size_t global[2] = {params.nx/3, params.ny/3};
+  size_t global[2] = {params.nx/4, params.ny/3};
   err = clEnqueueNDRangeKernel(ocl.queue, ocl.comp_func,
                                2, NULL, global, NULL, 0, NULL, NULL);
   checkError(err, "enqueueing comp_func kernel", __LINE__);
