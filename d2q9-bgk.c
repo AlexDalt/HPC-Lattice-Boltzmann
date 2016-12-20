5e642c936d988b3db449963a8b9ccaee23601d84
Alex Dalton <alex.k.dalton@gmail.com>
2016-12-20 04:16:11 +0000

    okay maybe this is the working reset

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -254,8 +254,6 @@ float comp_func(const t_param params, cl_mem* cells, cl_mem* tmp_cells, t_ocl oc
   checkError(err, "setting comp_func arg 5", __LINE__);
   err = clSetKernelArg(ocl.comp_func, 6, sizeof(cl_float), &params.omega);
   checkError(err, "setting comp_func arg 6", __LINE__);
-  err = clSetKernelArg(ocl.comp_func, 7, sizeof(t_speed) * size, NULL);
-  checkError(err, "setting comp_func arg 7", __LINE__);
 
   // Enqueue kernel
   size_t global[2] = {params.nx, params.ny};
